9275dec05631deac20952e9f02e3d041c4f79db6
Nico Diaz <nicodiazan@gmail.com>
2025-01-27 23:27:32 +0100

    Update the main page to add a very basic interface

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,48 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-import { parseHarlowe, runTests } from "../utils/parseHarlowe.ts";
+import { ref } from "vue";
+import parseHarlowe from "../utils/parseHarlowe.ts";
+import { runTests } from "../tests/tests.ts";
 
-// 🧪 **Test Example**
-const harloweCode = `(set: $name to "John")
-(if: $score > 10)[You win!]
-(link: "Next" -> "NextPassage")
-(a: 1, 2, 3)
-(dm: "name", "John", "score", 10)
-(m: 1 + 2)
-(live: 2s)[Time is running!]
-(history:)[You've visited (visited: "Room")]
-(print: "Your score is " + $score)
-`;
-
-const altCode = `
-(set: $name to "John Doe")
-(if: $score > 10)[You win!]
-(link: "Next" -> "NextPassage")
-(a: 1, 2, 3)
-(dm: "name", "John", "score", 10)
-(live: 2s)[Time is running!]
-(print: "Your score is " + $score)
-`;
-
-(altCode + harloweCode)[1] === "B" ? console.log("a") : console.log("b");
-
-// const parsedJSON = parseHarlowe(harloweCode);
-// console.log(JSON.stringify(parsedJSON, null, 2));
+const harloweCode = ref("");
+const outputJson = ref({});
 
 runTests();
+
+function updateHarloweCode(event: Event) {
+	harloweCode.value = (event.target as HTMLInputElement).value;
+}
+
+function updateOutputJson() {
+	outputJson.value = parseHarlowe(harloweCode.value);
+}
+// const parsedJSON = parseHarlowe(harloweCode);
+// console.log(JSON.stringify(parsedJSON, null, 2));
 </script>
 
 <template>
 	<div>
-		<a href="https://vite.dev" target="_blank">
-			<img src="/vite.svg" class="logo" alt="Vite logo" />
-		</a>
-		<a href="https://vuejs.org/" target="_blank">
-			<img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-		</a>
+		<h1>Harlowe parser</h1>
+		<div>
+			<div class="card">
+				<textarea
+					@input="updateHarloweCode"
+					placeholder="Enter Harlowe code here"
+					id="harloweInput"
+				/>
+				<pre id="harloweOutput">{{ outputJson }}</pre>
+			</div>
+			<button @click="updateOutputJson">Parse</button>
+		</div>
 	</div>
-	<HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-	height: 6em;
-	padding: 1.5em;
-	will-change: filter;
-	transition: filter 300ms;
+#harloweInput,
+#harloweOutput {
+	height: 30vh;
+	width: 30vw;
 }
-.logo:hover {
-	filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-	filter: drop-shadow(0 0 2em #42b883aa);
+.card {
+	display: flex;
 }
 </style>
