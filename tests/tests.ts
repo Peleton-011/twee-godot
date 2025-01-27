bb997e95b55ee238661f0bf74ac2cfe538dda644
Nico Diaz <nicodiazan@gmail.com>
2025-01-28 00:16:10 +0100

    Update tests file so you can choose what tests to run

--- tests/tests.ts
+++ tests/tests.ts
@@ -1,34 +1,6 @@
 import parseHarlowe from "../utils/parseHarlowe.ts";
 // Test cases
-function runTests() {
-	const tests = [
-		'(set: $companion to "Gallifrey")',
-		'(link: "Next" -> "NextPassage")',
-		"(if: $condition)",
-		'(print: "Hello")',
-		'(if: $condition) (print: "Hello")',
-		`(if: $condition) 
-        (print: "Hello")`,
-		`(set: $name to "John Doe")
-        (if: $score > 10)[You win!]
-        (link: "Next" -> "NextPassage")
-        (a: 1, 2, 3)
-        (dm: "name", "John", "score", 10)
-        (live: 2s)[Time is running!]
-        (print: "Your score is " + $score)
-        `,
-		`(set: $name to "John")
-        (if: $score > 10)[You win!]
-        (link: "Next" -> "NextPassage")
-        (a: 1, 2, 3)
-        (dm: "name", "John", "score", 10)
-        (m: 1 + 2)
-        (live: 2s)[Time is running!]
-        (history:)[You've visited (visited: "Room")]
-        (print: "Your score is " + $score)
-        `,
-	];
-
+function runTests(tests: string[]) {
 	tests.forEach((test) => {
 		try {
 			console.log("\nTesting:", test);
@@ -40,4 +12,53 @@ function runTests() {
 	});
 }
 
-export { runTests };
+const mainTests = [
+	'(set: $companion to "Gallifrey")',
+	'(link: "Next" -> "NextPassage")',
+	"(if: $condition)",
+	'(print: "Hello")',
+	'(if: $condition) (print: "Hello")',
+	`(if: $condition) 
+    (print: "Hello")`,
+	`(set: $name to "John Doe")
+    (if: $score > 10)[You win!]
+    (link: "Next" -> "NextPassage")
+    (a: 1, 2, 3)
+    (dm: "name", "John", "score", 10)
+    (live: 2s)[Time is running!]
+    (print: "Your score is " + $score)
+    `,
+	`(set: $name to "John")
+    (if: $score > 10)[You win!]
+    (link: "Next" -> "NextPassage")
+    (a: 1, 2, 3)
+    (dm: "name", "John", "score", 10)
+    (m: 1 + 2)
+    (live: 2s)[Time is running!]
+    (history:)[You've visited (visited: "Room")]
+    (print: "Your score is " + $score)
+    `,
+];
+
+const altTests = [
+	// Should pass
+	'(set: $name to "John")',
+	// Should pass
+	"(if: $score > 10)[You win!]",
+	// Should pass
+	'(link: "Next" -> "NextPassage")',
+	// Should pass
+	"(a: 1, 2, 3)",
+	// This might be the issue
+	'(dm: "name", "John", "score", 10)',
+	// Should pass
+	"(m: 1 + 2)",
+	// Should pass
+	"(live: 2s)[Time is running!]",
+	// This is likely problematic
+	'(history:)[You\'ve visited (visited: "Room")]',
+	// Should pass
+	'(print: "Your score is " + $score)',
+];
+
+export { runTests, mainTests, altTests };
